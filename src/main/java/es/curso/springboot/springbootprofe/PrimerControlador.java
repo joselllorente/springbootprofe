7a0b2f9a82a4b000de321068717f7d55818a2b89
jlllorente3 <joseluis.llorenteperales+3@gmail.com>
2025-05-27 17:15:28 +0200

    Operaciones requesta Spring MVC

--- src/main/java/es/curso/springboot/springbootprofe/PrimerControlador.java
+++ src/main/java/es/curso/springboot/springbootprofe/PrimerControlador.java
@@ -4,19 +4,57 @@ import java.time.LocalDate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping(value="/start")
 public class PrimerControlador {
 
-	@RequestMapping("/inicio")
-	public String metodo1 (Model model, HttpServletRequest req, LocalDate date) {
+//	@RequestMapping(value="/inicio", method=RequestMethod.GET)
+//	public String metodo1 (Model model, HttpServletRequest req, LocalDate date) {
+//		System.out.println("Mi primer controlador de Spring");
+//		String valor = req.getParameter("param1");
+//		model.addAttribute("mensaje", "Jose Luis Llorente ");
+//		model.addAttribute("param_valor", valor);
+//		return "hola";
+//	}
+	
+	@RequestMapping(value="/inicio", method=RequestMethod.GET)
+	public String metodo2 (Model model, @RequestParam("param1") String valor) {
+		System.out.println("Mi primer controlador de Spring");
+		model.addAttribute("mensaje", "Jose Luis Llorente ");
+		model.addAttribute("param_valor", valor);
+		return "hola";
+	}
+	
+	@RequestMapping(value="/inicio", method=RequestMethod.POST)
+	public String metodo3 (Model model, HttpServletRequest req, LocalDate date) {
 		System.out.println("Mi primer controlador de Spring");
 		String valor = req.getParameter("param1");
 		model.addAttribute("mensaje", "Jose Luis Llorente ");
 		model.addAttribute("param_valor", valor);
 		return "hola";
 	}
+	
+	@RequestMapping("/{generico}")
+	public String printHelloGet(ModelMap model, 
+			@PathVariable("generico") String accion, 
+			@RequestParam("numero") String param) {
+		
+		System.out.println("printHelloGet entra: "+ accion);
+		
+		if (accion.toUpperCase().equals("GET")){
+			model.addAttribute("message","Action get called with parameter "+param);
+		}else{
+			model.addAttribute("message","Action other called with parameter "+param);
+		}
+		return "hola";//Separado de la vista, se manda el valor pero no se sabe donde se va a mostrar. ME Abstraigo de la tecnologia que hay por debajo
+	}
+	
 }
